1a0e76ad3fb9350146e58173ee5c09483631cf88
Andy <nnlyx@hotmail.com>
2016-08-26 22:02:59 -0600

    Init

---
+++ app/app.routes.ts
@@ -0,0 +1,16 @@
+import { provideRouter, RouterConfig } from '@angular/router';
+
+import { AboutComponent, AboutHomeComponent, AboutItemComponent } from 'app/about.component';
+import { HomeComponent } from 'app/home.component';
+
+export const routes: RouterConfig = [
+  { path: '', component: HomeComponent },
+  {
+    path: 'about',
+    component: AboutComponent,
+    children: [
+      { path: '', component: AboutHomeComponent },
+      { path: 'item/:id', component: AboutItemComponent }
+    ]
+  }
+];
---
+++ app/home.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `<h2>Home</h2> `
+})
+export class HomeComponent { 
+  
+}
